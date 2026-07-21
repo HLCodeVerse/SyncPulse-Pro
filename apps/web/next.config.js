f0048ca43e291ac03e62e785244a48a05581cbd7
hless4326 <hless4326@gmail.com>
2026-07-22 00:02:41 +0530

    fix: Add tsconfig paths & Webpack aliases for @webrtc packages to fix Vercel module resolution

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@webrtc/ui', '@webrtc/sdk', '@webrtc/types'],
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@webrtc/types': path.resolve(__dirname, '../../packages/types/src/index.ts'),
+      '@webrtc/sdk': path.resolve(__dirname, '../../packages/webrtc-sdk/src/index.ts'),
+      '@webrtc/ui': path.resolve(__dirname, '../../packages/ui/src/index.ts'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
